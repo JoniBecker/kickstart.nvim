e21a7581c906e6882ba830ebba37a3d89ea7861c
Johannes Becker <aemkaydude@gmail.com>
2025-07-28 12:40:11 +0200

    Added codelens and activated some lsps, and lsp-config for haskell

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -100,9 +100,7 @@ vim.g.have_nerd_font = false
 
 -- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -585,6 +583,17 @@ require('lazy').setup({
             })
           end
 
+          -- TODO Put this somewhere reasonable
+          -- ALso this is Joni garbage code
+          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_codeLens) then
+            vim.api.nvim_create_autocmd({ 'BufEnter', 'InsertLeave' }, {
+              callback = function()
+                vim.lsp.codelens.refresh()
+              end,
+            })
+            vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, { desc = 'Run CodeLens actions' })
+          end
+
           -- The following code creates a keymap to toggle inlay hints in your
           -- code, if the language server you are using supports them
           --
@@ -614,6 +623,17 @@ require('lazy').setup({
         stylua = {}, -- Used to format Lua code
 
         -- Special Lua Config, as recommended by neovim help docs
+        hls = {
+          cmd = { '/home/joni/.ghcup/bin/haskell-language-server-wrapper', '--lsp' },
+          filetypes = { 'haskell', 'lhaskell' },
+          settings = {
+            haskell = {
+              formattingProvider = 'fourmolu',
+            },
+          },
+        },
+        gopls = {},
+        ts_ls = {},
         lua_ls = {
           on_init = function(client)
             client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
